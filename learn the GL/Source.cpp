4d1aff55e4cce0fa5a548e9ee24e5e2f047c5059
Mansen <50342436+mansen420@users.noreply.github.com>
2023-09-18 15:01:54 +0300

    added diffusion and specular maps

--- learn the GL/Source.cpp
+++ learn the GL/Source.cpp
@@ -59,51 +59,72 @@ int main()
     -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 0.0f,  1.0f,  0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f,  0.0f
     };
-
+    //buffer object instantiation and vertex attribute configuration 
     VAO vao0(true);
     vao0.bind(); 
     vao0.bindVBO();
     vao0.vertexBufferData(vertices, GL_STATIC_DRAW, sizeof(vertices));
-
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(5 * sizeof(float)));
     glEnableVertexAttribArray(2);
+
     VAO vao1LIGHT(true);
     vao1LIGHT.bind();
-    vao1LIGHT.addVBO(&vao0.vertexBufferObject);     //no need to bind again apparently 
+    vao1LIGHT.overwriteVBO(&vao0.vertexBufferObject);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-//    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-//    glEnableVertexAttribArray(1);
-
+    //texture loading 
     stbi_set_flip_vertically_on_load(true);
     int height, width, nrChannels;
-    unsigned char* data = stbi_load("C:/Users/msi/Desktop/wall.jpg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("C:/Users/msi/Desktop/container2.PNG", &width, &height, &nrChannels, 0);
+
     unsigned int texture0;
     if (data)
     {
         glGenTextures(1, &texture0);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture0);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
         std::cout << "loading texture failed" << std::endl;
-
+    data = 0;
+    data = stbi_load("C:/Users/msi/Desktop/container2_specular.PNG", &width, &height, &nrChannels, 0);
+    unsigned int texture1;
+    if (data)
+    {
+        glGenTextures(1, &texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+        std::cout << "loading texture failed" << std::endl;
+    //shader configuration 
     Shader shaders("C:/Users/msi/source/repos/learn the GL/learn the GL/vertexShader.GLSL",
                    "C:/Users/msi/source/repos/learn the GL/learn the GL/fragmentShader.GLSL");
     shaders.use();      //use() before setting uniforms
-    glUniform1i(glGetUniformLocation(shaders.ID, "texture0"), 0);
-    glUniform3f(glGetUniformLocation(shaders.ID, "objectColor"), 0.9, 0.5, 0.31);
+    glUniform1i(glGetUniformLocation(shaders.ID, "defaultMaterial.diffuse"), 0);
+    glUniform1i(glGetUniformLocation(shaders.ID, "defaultMaterial.specular"), 1);
+    //object material values
+    glUniform3f(glGetUniformLocation(shaders.ID, "defaultMaterial.ambient"), 1.0f, 0.5f, 0.31f);
+    glUniform3f(glGetUniformLocation(shaders.ID, "defaultMaterial.diffuse"), 1.0f, 0.5f, 0.31f);
+    glUniform3f(glGetUniformLocation(shaders.ID, "defaultMaterial.specular"), 0.5f, 0.5f, 0.5f);
+    glUniform1f(glGetUniformLocation(shaders.ID, "defaultMaterial.shininess"), 32.0f);
+    //light values
+    glUniform3f(glGetUniformLocation(shaders.ID, "cubeLight.ambient"), 0.2f, 0.2f, 0.2f);
+    glUniform3f(glGetUniformLocation(shaders.ID, "cubeLight.diffuse"), 0.5f, 0.5f, 0.5f);
+    glUniform3f(glGetUniformLocation(shaders.ID, "cubeLight.specular"), 1.0f, 1.0f, 1.0f);
 
     Shader shaderLIGHT("C:/Users/msi/source/repos/learn the GL/learn the GL/vertexShader.GLSL",
         "C:/Users/msi/source/repos/learn the GL/fragmentShaderLIGHT.GLSL");
 
-
+    //render loop
     glm::mat4 view = glm::mat4(1.0f);
     Camera cam(myWindow, &view);
     glEnable(GL_DEPTH_TEST);
@@ -118,14 +139,13 @@ int main()
 
         glUseProgram(shaders.ID);
         glm::vec3 lightPos(3.0f*cos(0.25*glfwGetTime()), 3.0f * sin(0.25*glfwGetTime()), 0.0f);
-        glUniform3f(glGetUniformLocation(shaders.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+        glUniform3f(glGetUniformLocation(shaders.ID, "cubeLight.position"), lightPos.x, lightPos.y, lightPos.z);
         glUniformMatrix4fv(glGetUniformLocation(shaders.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(glGetUniformLocation(shaders.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(glGetUniformLocation(shaders.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
         glBindVertexArray(vao0.getID());
 
-        glBindTexture(GL_TEXTURE_2D, texture0);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         shaderLIGHT.use();
