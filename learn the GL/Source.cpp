a41bc088e0715fe1952b62a630458ac9ede51804
Mansen <50342436+mansen420@users.noreply.github.com>
2023-09-22 03:02:20 +0300

    fixed issue with some includes

--- learn the GL/Source.cpp
+++ learn the GL/Source.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "SHADER.h"
 #define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
